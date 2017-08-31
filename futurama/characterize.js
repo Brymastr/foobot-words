1d1a8af9fae0f8c4a4a883aa6162032d61dd61e7
Brycen <dorsay@live.ca>
2017-08-31 00:40:47 -0700

    futurama corpus

--- futurama/characterize.js
+++ futurama/characterize.js
@@ -1,1 +1,31 @@
-// placeholder
+const
+  { promisify } = require('util'),
+  fs = require('fs'),
+  readdir = promisify(fs.readdir),
+  endOfLine = require('os').EOL;
+
+
+
+main();
+
+async function main() {
+  const dir = './corpus/'
+  const files = await readdir(dir);
+  await writeCharacter('bender', files);
+}
+
+async function writeCharacter(character, files) {
+  const path = './corpus/bender.txt';
+  const writeStream = fs.createReadStream(path);
+
+  for(const file of files) {
+    const contents = await parseFile(character, file);
+    contents.split(endOfLine).forEach(line => {
+      
+    });
+  }
+}
+
+async function parseFile(character, file) {
+
+} 
